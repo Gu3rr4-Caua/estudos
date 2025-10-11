94658fa24aca2b0cb407743ff8bc14230a306910
Guerra <cauaguerra33@gmail.com>
2025-10-11 13:20:22 +0000

    Adiciona a pasta page e alteração no index.js

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home(){
+    return <h1>Eu gostaria de te dizer que apesar dos nossos problemas, eu quero que no final
+        da caminhada você esteja lá. Amo você S2
+    </h1>
+}
+
+export default Home;
