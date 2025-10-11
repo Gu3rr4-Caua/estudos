27c211bfe4d5759e37f274850e38451fdba45047
Guerra <cauaguerra33@gmail.com>
2025-10-11 15:24:16 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home(){
     return <h1>Eu gostaria de te dizer que apesar dos nossos problemas, eu quero que no final
-        da caminhada você esteja lá. Amo você S2
+        da caminhada você esteja lá. Amo você S2 ❤️
     </h1>
 }
 
